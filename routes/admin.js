f70eb98e8d762f382a306d46e1988e6777016494
malaniharshita <malaniharshita@gmail.com>
2024-03-28 00:03:23 +0530

    controllers

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 router.get('/add-product',(req,res,next) => {
     //console.log('in the middleware');
-    res.send('<form action = "/product" method = "POST">Name:<input type = "text" name = "title">Size:<input type = "text" name = "size"><button type = "submit">Add Product</button></form>');
+    res.send('<form action = "/add-product" method = "POST">Name:<input type = "text" name = "title">Size:<input type = "text" name = "size"><button type = "submit">Add Product</button></form>');
 })
 
-router.post('/product',(req,res,next) => {
+router.post('/add-product',(req,res,next) => {
     console.log(req.body);
     res.redirect('/');
 })
